1beb2e3f476539f5acba4a152c5d99c4fba2c3a8
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-02-12 02:34:23 +0530

    Javascript dom practice challenge 6 completed

---
+++ script-2.js
@@ -0,0 +1,46 @@
+// const homeTab = document.getElementById("home-tab");
+// const aboutTab = document.getElementById("about-tab");
+// const contactTab = document.getElementById("contact-tab");
+
+// const homeText = document.getElementById("home-text");
+// const aboutText = document.getElementById("about-text");
+// const contactText = document.getElementById("contact-text");
+
+// const removeAllText = () => {
+//   document
+//     .querySelectorAll(".text")
+//     .forEach((item) => (item.style.display = "none"));
+// };
+
+// removeAllText();
+
+// homeTab.addEventListener("click", () => {
+//   removeAllText();
+//   homeText.style.display = "block";
+// });
+// aboutTab.addEventListener("click", () => {
+//   removeAllText();
+//   aboutText.style.display = "block";
+// });
+// contactTab.addEventListener("click", () => {
+//   removeAllText();
+//   contactText.style.display = "block";
+// });
+
+const tabs = ["home", "about", "contact"];
+
+document
+  .querySelectorAll(".text")
+  .forEach((item) => (item.style.display = "none"));
+
+tabs.forEach((tab) => {
+  const tabElement = document.getElementById(`${tab}-tab`);
+  const textElement = document.getElementById(`${tab}-text`);
+
+  tabElement.addEventListener("click", () => {
+    document
+      .querySelectorAll(".text")
+      .forEach((item) => (item.style.display = "none"));
+    textElement.style.display = "block";
+  });
+});
