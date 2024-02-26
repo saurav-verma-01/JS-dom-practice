7f6ef5097668b6d914426cdb9c9f9788a5796c80
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-02-26 20:23:10 +0530

    challenge 7 completed

--- script-2.js
+++ script-2.js
@@ -27,20 +27,35 @@
 //   contactText.style.display = "block";
 // });
 
-const tabs = ["home", "about", "contact"];
+// const tabs = ["home", "about", "contact"];
 
-document
-  .querySelectorAll(".text")
-  .forEach((item) => (item.style.display = "none"));
+// document
+//   .querySelectorAll(".text")
+//   .forEach((item) => (item.style.display = "none"));
 
-tabs.forEach((tab) => {
-  const tabElement = document.getElementById(`${tab}-tab`);
-  const textElement = document.getElementById(`${tab}-text`);
+// tabs.forEach((tab) => {
+//   const tabElement = document.getElementById(`${tab}-tab`);
+//   const textElement = document.getElementById(`${tab}-text`);
 
-  tabElement.addEventListener("click", () => {
-    document
-      .querySelectorAll(".text")
-      .forEach((item) => (item.style.display = "none"));
-    textElement.style.display = "block";
-  });
-});
+//   tabElement.addEventListener("click", () => {
+//     document
+//       .querySelectorAll(".text")
+//       .forEach((item) => (item.style.display = "none"));
+//     textElement.style.display = "block";
+//   });
+// });
+
+const bar = document.querySelector(".download-bar");
+const h3 = document.getElementById("download-Heading");
+
+let widthPercent = 0;
+
+const interval = setInterval(() => {
+  if (widthPercent === 100) {
+    h3.style.opacity = 1;
+    clearInterval(interval);
+  }
+
+  widthPercent++;
+  bar.style.width = `${widthPercent}%`;
+}, 50);
